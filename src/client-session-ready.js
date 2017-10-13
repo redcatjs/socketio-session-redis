4de09cba329148304fb93060eb0c4da53e50e65e
jo <jo@surikat.pro>
2017-10-13 15:07:26 +0200

    add openSessionCallback and socketConnected

--- src/client-session-ready.js
+++ src/client-session-ready.js
@@ -1,13 +1,26 @@
 import Cookies from 'cookies-js'
-function ClientSessionReady(socket){
+function ClientSessionReady(socket, openSessionCallback){
+	let resolved = false;
+	let socketConnected = null;
 	return new Promise((resolve, reject)=>{
 		socket.on('connect',()=>{
 			socket.emit('openSession', null, answer => {
 				if(answer!==true){
 					Cookies.set('token',answer);
 				}
-				resolve(answer);
+				let reconnecting = socketConnected===false;
+				socketConnected = true;
+				if(openSessionCallback){
+					openSessionCallback(answer, reconnecting);
+				}
+				if(!resolved){					
+					resolved = true;
+					resolve(answer);
+				}
 			});			
+		});
+		socket.on('disconnect',()=>{
+			socketConnected = false;
 		});
 	});
 }
